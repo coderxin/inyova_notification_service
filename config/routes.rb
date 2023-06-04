f4c27f8d19e91d3086c74f3a46584c38698f5706
Andrejs Eisaks <eisaks.andrejs@gmail.com>
2023-06-05 01:16:49 +0200

    feat(Client notifications): add NotificationsController and BaseController with auth restriction

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       post :sessions, to: 'sessions#create'
       delete :sessions, to: 'sessions#destroy'
+
+      resources :notifications, only: [:index, :show]
     end
   end
 end
