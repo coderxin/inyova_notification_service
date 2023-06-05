bb48feca9e9e569e6f5c47101e2de777fdbc5e77
Andrejs Eisaks <eisaks.andrejs@gmail.com>
2023-06-05 03:10:47 +0200

    chore(Rspec): restore `GET /show` spec for the `api/v1/admin/notifications` endpoint

--- spec/requests/api/v1/admin/notifications_spec.rb
+++ spec/requests/api/v1/admin/notifications_spec.rb
@@ -148,48 +148,45 @@ RSpec.describe '/api/admin/v1/notifications', type: :request do
     end
   end
 
-  # describe 'GET /show' do
-  #   context 'when request is authenticated' do
-  #     context 'when notification is not assigned to current user' do
-  #       it 'renders a successful response' do
-  #         notification = create(:notification)
-  #
-  #         get api_v1_notification_url(notification), headers: valid_headers, as: :json
-  #
-  #         expect(response.status).to eq(404)
-  #         expect(response_json).to eq({ 'error' => 'Not Found' })
-  #       end
-  #     end
-  #
-  #     context 'when notification is assigned to current user' do
-  #       it 'renders a successful response' do
-  #         notification = create(:notification)
-  #         create(:notification_assignment, notification:, user:)
-  #
-  #         get api_v1_notification_url(notification), headers: valid_headers, as: :json
-  #
-  #         expect(response.status).to eq(200)
-  #         expect(response_json).to eq({
-  #                                       'data' => {
-  #                                         'id' => notification.id,
-  #                                         'date' => notification.date.to_s,
-  #                                         'title' => notification.title,
-  #                                         'description' => notification.description,
-  #                                         'created_at' => notification.created_at.iso8601(3),
-  #                                         'updated_at' => notification.updated_at.iso8601(3)
-  #                                       }
-  #                                     })
-  #       end
-  #     end
-  #   end
-  #
-  #   context 'when request is not authenticated' do
-  #     it 'renders an unsuccessful response' do
-  #       get api_v1_notifications_url(1), headers: {}, as: :json
-  #
-  #       expect(response.status).to eq(401)
-  #       expect(response_json).to eq({ 'error' => 'Not authorized' })
-  #     end
-  #   end
-  # end
+  describe 'GET /show' do
+    context 'when request is authenticated' do
+      context 'when notification is not assigned to current user' do
+        it 'renders an unsuccessful response' do
+          get api_v1_admin_notification_url(123), headers: valid_headers, as: :json
+
+          expect(response.status).to eq(404)
+          expect(response_json).to eq({ 'error' => 'Not Found' })
+        end
+      end
+
+      context 'when notification is assigned to current user' do
+        it 'renders a successful response' do
+          notification = create(:notification)
+
+          get api_v1_admin_notification_url(notification), headers: valid_headers, as: :json
+
+          expect(response.status).to eq(200)
+          expect(response_json).to eq({
+                                        'data' => {
+                                          'id' => notification.id,
+                                          'date' => notification.date.to_s,
+                                          'title' => notification.title,
+                                          'description' => notification.description,
+                                          'created_at' => notification.created_at.iso8601(3),
+                                          'updated_at' => notification.updated_at.iso8601(3)
+                                        }
+                                      })
+        end
+      end
+    end
+
+    context 'when request is not authenticated' do
+      it 'renders an unsuccessful response' do
+        get api_v1_admin_notification_url(1), headers: {}, as: :json
+
+        expect(response.status).to eq(401)
+        expect(response_json).to eq({ 'error' => 'Not authorized' })
+      end
+    end
+  end
 end
