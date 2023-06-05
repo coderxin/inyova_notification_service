5d1bccc4669893fcc97c2d8992c3e1213ef75270
Andrejs Eisaks <eisaks.andrejs@gmail.com>
2023-06-05 03:59:33 +0200

    chore(Cleanup): add missing seeds and extend Postman collection

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,34 @@
 
 require 'securerandom'
 
-User.create(
+notification_future = Notification.create(
+  title: 'New ticker is available: TSLA',
+  description: 'Rich description of the TSLA ticker',
+  date: Date.tomorrow
+)
+
+notification_today = Notification.create(
+  title: 'New ticker is available: NFLX',
+  description: 'Rich description of the NFLX ticker',
+  date: Date.today
+)
+
+notification_past = Notification.create(
+  title: 'New ticker is available: AMZN',
+  description: 'Rich description of the AMZN ticker',
+  date: Date.yesterday
+)
+
+user = User.create(
   email: 'with_notifications@inyova.com',
   password: 'inyova1',
   password_confirmation: 'inyova1',
   device_token: SecureRandom.hex(8)
 )
+
+user.notifications << notification_past
+user.notifications << notification_today
+user.notifications << notification_future
 
 User.create(
   email: 'without_notifications@inyova.com',
@@ -23,3 +45,23 @@ User.create(
   password_confirmation: 'inyova1',
   device_token: SecureRandom.hex(8)
 )
+
+notification_today = Notification.create(
+  title: 'New ticker is available: UBSG',
+  description: 'Rich description of the UBSG ticker',
+  date: Date.today
+)
+
+(0..50).each do |i|
+  user = User.create(
+    email: "test#{i}@inyova.com",
+    password: 'inyova1',
+    password_confirmation: 'inyova1',
+    device_token: SecureRandom.hex(8)
+  )
+
+  notification_today.users << user
+end
+
+# To schedule notification_today run in rails console:
+# Notifications::SchedulingService.new(Notification.last).call
